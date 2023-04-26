51c2e727e1c17e468caec588f77c5613ce5a9d51
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-04-26 18:08:36 +0200

    Add a prefix for the cache keys

--- streamlit_session_memo/session_memo.py
+++ streamlit_session_memo/session_memo.py
@@ -1,12 +1,16 @@
+import uuid
 from typing import Hashable
 
 import streamlit as st
 
 
+CACHE_KEY_PREFIX = uuid.uuid4().hex  # To make the key more unique even when args or kwargs are so simple
+
+
 def calc_cache_key(args, kwargs) -> Hashable:
     hashable_args = [a if isinstance(a, Hashable) else id(a) for a in args]
     hashable_kwargs = {k: v if isinstance(v, Hashable) else id(v) for k, v in kwargs.items()}
-    return tuple(hashable_args), tuple(sorted(hashable_kwargs.items()))
+    return CACHE_KEY_PREFIX, tuple(hashable_args), tuple(sorted(hashable_kwargs.items()))
 
 
 def session_memo(func):
