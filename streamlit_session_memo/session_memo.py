da551f58685acd0f9429d08aaaaf342019597bd3
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-04-27 13:58:49 +0200

    Include the cached function name to the cache key

--- streamlit_session_memo/session_memo.py
+++ streamlit_session_memo/session_memo.py
@@ -7,10 +7,16 @@ import streamlit as st
 CACHE_KEY_PREFIX = uuid.uuid4().hex  # To make the key more unique even when args or kwargs are so simple
 
 
-def calc_cache_key(args, kwargs) -> Hashable:
+def get_fully_qualified_name(func):
+    module_name = func.__module__
+    qualname = func.__qualname__
+    return f"{module_name}.{qualname}"
+
+
+def calc_cache_key(func, args, kwargs) -> Hashable:
     hashable_args = [a if isinstance(a, Hashable) else id(a) for a in args]
     hashable_kwargs = {k: v if isinstance(v, Hashable) else id(v) for k, v in kwargs.items()}
-    return CACHE_KEY_PREFIX, tuple(hashable_args), tuple(sorted(hashable_kwargs.items()))
+    return CACHE_KEY_PREFIX, get_fully_qualified_name(func), tuple(hashable_args), tuple(sorted(hashable_kwargs.items()))
 
 
 def st_session_memo(func):
--- tests/test_session_memo.py
+++ tests/test_session_memo.py
@@ -5,8 +5,7 @@ import pytest
 from streamlit_session_memo.session_memo import calc_cache_key
 
 
-class TestCalcCacheKey:
-    @pytest.mark.parametrize("args, kwargs", [
+TEST_ARGS_LIST = [
         ((), {}),
         ((None,), {}),
         ((1,), {}),
@@ -18,11 +17,31 @@ class TestCalcCacheKey:
         ((), {"a": "b"}),
         ((), {"a": None}),
         ((1, 2, 3), {'a': 1, 'b': 2, 'c': 3})
-    ])
+]
+
+
+class TestCalcCacheKey:
+    @pytest.mark.parametrize("args, kwargs", TEST_ARGS_LIST)
     def test_hashable_arguments(self, args, kwargs):
-        assert calc_cache_key(args, kwargs) == calc_cache_key(args, kwargs)
-        assert isinstance(calc_cache_key(args, kwargs), typing.Hashable)
+        def foo():
+            pass
+
+        assert calc_cache_key(foo, args, kwargs) == calc_cache_key(foo, args, kwargs)
+        assert isinstance(calc_cache_key(foo, args, kwargs), typing.Hashable)
+
+    @pytest.mark.parametrize("args, kwargs", TEST_ARGS_LIST)
+    def test_with_different_functions(self, args, kwargs):
+        def foo(a, b):
+            pass
+
+        def bar(a, b):
+            pass
+
+        assert calc_cache_key(foo, args, kwargs) != calc_cache_key(bar, args, kwargs)
 
     def test_unhashable_arguments(self):
-        assert calc_cache_key((object(),), {}) != calc_cache_key((object(),), {})
-        assert isinstance(calc_cache_key((object(),), {}), typing.Hashable)
+        def foo():
+            pass
+
+        assert calc_cache_key(foo, (object(),), {}) != calc_cache_key(foo, (object(),), {})
+        assert isinstance(calc_cache_key(foo, (object(),), {}), typing.Hashable)
